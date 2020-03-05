411ea0faae13fbb154401d0654b7d5170fd4e36c
Andrés Santana <drew.lizcano@gmail.com>
2020-03-05 00:04:08 -0500

    servir react con express

--- Proyectos/PlatziVideo/client/webpack.config.js
+++ Proyectos/PlatziVideo/client/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -8,7 +7,8 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'assets/app.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -58,12 +58,9 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    }),
+    
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css'
+      filename: 'assets/app.css'
     }),
   ]
 };
