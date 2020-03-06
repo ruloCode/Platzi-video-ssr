2872323911701042903524f52bd239297ab97e27
Andrés Santana <drew.lizcano@gmail.com>
2020-03-06 12:34:59 -0500

    SSR

--- Proyectos/PlatziVideo/client/src/server/server.js
+++ Proyectos/PlatziVideo/client/src/server/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import { config } from '../../config/index';
-import webpack from 'webpack'
+import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import initialState from '../frontend/initialState';
+import reducer from '../frontend/reducers/index';
 const app = express()
 
 
@@ -12,29 +21,49 @@ if (config.dev === 'development') {
     const webpackHotMiddleware = require('webpack-hot-middleware');
 
     const compiler = webpack(webpackConfig);
-    const serverConfig = { port: config.port, hot: true};
+    const serverConfig = { 
+        contentBase: `http://localhost${config.port}`,
+        port: config.port, 
+        publicPath: webpackConfig.output.publicPath,
+        hot: true,
+        historyApiFallback: true,
+        stats: { colors: true },
+
+    };
     app.use(webpackDevMiddleware(compiler, serverConfig))
     app.use(webpackHotMiddleware(compiler))
 
 
 }
-
-
-app.get('*',(req, res)=>{
-    res.send(`
-    <!DOCTYPE html>
+const setResponse = (html) => {
+    return (`
+        <!DOCTYPE html>
         <html>
             <head>
                 <link rel="stylesheet" href="assets/app.css" type="text/css">
                 <title>Platzi Video</title>
             </head>
             <body>
-                <div id="app"></div>
+                <div id="app">${html}</div>
                 <script src="assets/app.js" type="text/javascript" ></script>
             </body>
         </html>
     `)
-})
+}
+const renderApp= (req, res) => {
+    const store = createStore(reducer, initialState);
+    const html = renderToString(
+        <Provider store={store}>
+            <StaticRouter location={req.url} context={{}}>
+                {renderRoutes(serverRoutes)}
+            </StaticRouter>
+      </Provider>,
+    );
+    res.send(setResponse(html));  
+}
+
+
+app.get('*', renderApp)
 
 app.listen(config.port, (err)=>{
     if (err) {
